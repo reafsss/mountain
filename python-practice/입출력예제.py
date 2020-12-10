8caef7e02e302a589539c4ad06c54c51e19ce366
DEOKSEONG KIM <reafsss@naver.com>
2020-12-10 22:33:51 +0900

    add a new file: InputandPrint-practice

---
+++ python-practice/입출력예제.py
@@ -0,0 +1,17 @@
+#이름, 핸드폰번호(숫자만), 좋아하는 음식을 입력 받아서 출력하는 프로그램 작성
+userInput_name=input("이름: ")
+userInput_phone=input("핸드폰 번호: ")
+userInput_food=input("좋아하는 음식: ")
+
+print("이름:", userInput_name)
+print("핸드폰 번호: ", userInput_phone)
+print("좋아하는 음식: ", userInput_food)
+
+#심화-> for문을 활용해 작성
+# userInputDetail = []
+
+# for i in range(3):
+#     a = input("이름, 핸드폰번호, 좋아하는음식을 차례대로 입력하세요: ")
+#     userInputDetail.append(a)
+
+# print(userInputDetail)
