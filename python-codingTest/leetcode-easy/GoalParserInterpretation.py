73c55931ab8350705a8b8de101c0acf4eb3d7416
DEOKSEONG KIM <reafsss@naver.com>
2020-12-13 21:57:54 +0900

    add a new codingtest file

---
+++ python-codingTest/leetcode-easy/GoalParserInterpretation.py
@@ -0,0 +1,9 @@
+# Test
+command = "G()(al)" #Input
+command_replaced = command.replace('()', 'o')
+command_replaced.replace('(al)', 'al') #Output
+
+# class Solution:
+#     def interpret(self, command: str) -> str:
+#         command_replaced = command.replace('()', 'o')
+#         return command_replaced.replace('(al)', 'al')
