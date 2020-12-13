48e7feb3b19846459f5627be5fe42b8f467fc7fd
DEOKSEONG KIM <reafsss@naver.com>
2020-12-13 13:34:15 +0900

    add a new codingTest file

---
+++ python-codingTest/leetcode-easy/DefanginganIPAddress.py
@@ -0,0 +1,8 @@
+# Test
+
+address = "1.1.1.1" #Input
+address.replace('.', '[.]') #Output
+
+# class Solution:
+#     def defangIPaddr(self, address: str) -> str:
+#         return address.replace('.', '[.]')
