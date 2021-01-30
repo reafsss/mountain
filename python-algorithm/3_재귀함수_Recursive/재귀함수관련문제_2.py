a7c664f79fafb67c7eb27c560c8853bdbcfe6294
DEOKSEONG KIM <reafsss@naver.com>
2021-01-30 23:54:58 +0900

    change a file

--- python-algorithm/3_재귀함수_Recursive/재귀함수관련문제_2.py
+++ python-algorithm/3_재귀함수_Recursive/재귀함수관련문제_2.py
@@ -1,17 +1,35 @@
 # 재귀함수_2
 # 별 찍기
+# 별 찍는 재귀 함수
 
-num = int(input())
-
-num = 9
-
-def star(num):
-    print('***')
-    print('* *')
-    print('***')
-    return star_recursive(int(num/3))
-
-def star_recursive(num):
-    if num == 1:
+def draw_star(n) :
+    global Map
+    
+    if n == 3 :
+        Map[0][:3] = Map[2][:3] = [1]*3
+        Map[1][:3] = [1, 0, 1]
         return
-    return star(num)
+
+    a = n//3
+    draw_star(n//3)
+    for i in range(3) :
+        for j in range(3) :
+            if i == 1 and j == 1 :
+                continue
+            for k in range(a) :
+                Map[a*i+k][a*j:a*(j+1)] = Map[k][:a] # 핵심 아이디어
+
+N = int(input())      
+
+# 메인 데이터 선언
+Map = [[0 for i in range(N)] for i in range(N)]
+
+draw_star(N)
+
+for i in Map :
+    for j in i :
+        if j :
+            print('*', end = '')
+        else :
+            print(' ', end = '')
+    print()
